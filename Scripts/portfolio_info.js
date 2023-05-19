895d925563102a7c192ee2dc1769dcb5ff76cb6a
SohamSinghal <sohamsinghal32@gmail.com>
2023-05-20 00:34:48 +0530

    Final

---
+++ Scripts/portfolio_info.js
@@ -0,0 +1,45 @@
+import { createClient } from '@supabase/supabase-js'
+// Create a single supabase client for interacting with your database
+const supabase = createClient('https://zcdxtvqumrhlbnzlohzv.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpjZHh0dnF1bXJobGJuemxvaHp2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODQ0MDU1OTcsImV4cCI6MTk5OTk4MTU5N30.iCmc-OwXj_g9A60y3YzYpKvUXdrnOGHptmAAgnWt--I');
+//Projects
+var a = 0;
+const { data:projects, error:p } = await supabase.from("projects").select('*');
+function displayProjectInfo(index){
+document.getElementById('project_name').innerHTML = {data:projects}["data"][index]["Name"];
+document.getElementById('project_details').innerHTML = {data:projects}["data"][index]["Description"];
+document.getElementById('project_link').href = {data:projects}["data"][index]["Link"];
+}
+displayProjectInfo(a);
+document.getElementById('left_button').onclick = function() {
+    if(a == 0){
+        a = {data:projects}.length -1 ;
+    }
+    else{
+    a -= 1;}
+    displayProjectInfo(a);
+}
+document.getElementById('right_button').onclick = function() {
+    if(a == {data:projects}.length-1){
+        a = 0 ;
+    }
+    else{
+    a += 1;}
+    displayProjectInfo(a);
+}
+//Certificates
+const { data:accomplishments, error:acc } = await supabase.from("accomplishments").select('*');
+var inhtml = '';
+for(var i = 0;i< {data:accomplishments}["data"].length;i++)
+{
+    inhtml += '<li><a href = \' '+{data:accomplishments}["data"][i]["Certificate Link"]+'\'>'+{data:accomplishments}["data"][i]["Name"]+'</a></li>';
+}
+document.getElementById("certi").innerHTML = inhtml;
+//Work History
+const { data:workHist, error:wh } = await supabase.from("workHist").select('*');
+var inhtml = '';
+for(var i = 0;i< {data:workHist}["data"].length;i++)
+{
+    inhtml += '<h3>'+{data:workHist}["data"][i]["position name"]+'</h3><h4>'+{data:workHist}["data"][i]["company name"]+'</h4><h4>'+{data:workHist}["data"][i]["date"]+'</h4><p>' + {data:workHist}["data"][i]["description"]+'</p><a href = \"'+{data:workHist}["data"][i]["certification"]+'\">Certificate Link</a>'
+}
+console.log({data:workHist});
+document.getElementById("workHist").innerHTML = inhtml;
---
+++ main.js
@@ -0,0 +1,155 @@
+import * as THREE from 'three';
+import '/style.css';
+
+//Scene
+const scene = new THREE.Scene();
+//Camera and renderer
+const camera = new THREE.PerspectiveCamera(70,window.innerWidth/window.innerHeight,0.1,1000);
+camera.position.z = 0;
+const renderer = new THREE.WebGLRenderer({
+    canvas: document.querySelector('#bg'),
+  });  
+renderer.setSize(window.innerWidth,window.innerHeight);
+document.body.appendChild(renderer.domElement);
+//Light
+const light = new THREE.AmbientLight(0xffffff ,0.55);
+scene.add(light);
+light.position.set(200,0,0);
+
+//Variables
+const vertices = [];
+let velocities = [];
+const accelerations = [];
+var stars;
+const starGeo = new THREE.BufferGeometry();
+var moon = new THREE.Mesh();
+var jupiter = new THREE.Mesh();
+let cube;
+
+
+//Add Cube
+function addCube()
+{
+    let cubeGeometry = new THREE.BoxGeometry(0.5,0.5,0.5);
+    let loader = new THREE.TextureLoader();
+    let materialArray = [
+    new THREE.MeshStandardMaterial( { map: loader.load("asset/soham.png") } ),
+    new THREE.MeshStandardMaterial( { map: loader.load("asset/soham.png") } ),
+    new THREE.MeshStandardMaterial( { map: loader.load("asset/soham.png") } ),
+    new THREE.MeshStandardMaterial( { map: loader.load("asset/soham.png") } ),
+    new THREE.MeshStandardMaterial( { map: loader.load("asset/soham.png") } ),
+    new THREE.MeshStandardMaterial( { map: loader.load("asset/soham.png") } ),
+];
+    cube = new THREE.Mesh( cubeGeometry, materialArray );
+    cube.position.set(0.75,0,camera.position.z + 6);
+    scene.add( cube );
+}
+//Add stars
+function addStar(){
+for(let i = 0;i<10000;i++)
+{
+    vertices.push(Math.random() * 600 - 300, Math.random() * 600 - 300, Math.random() * 600 - 300);
+    velocities.push( 0 );
+    accelerations.push(0.00001);
+}
+starGeo.setAttribute('position', new THREE.Float32BufferAttribute(vertices, 3));
+const sprite = new THREE.TextureLoader().load('asset/star.png');
+const starMaterial = new THREE.PointsMaterial({color:0xffffff,size:0.4,map:sprite});
+stars = new THREE.Points(starGeo,starMaterial);
+scene.add(stars);
+}
+//Animate stars movement
+function starAnimations()
+{
+    const posAtr = starGeo.getAttribute('position');
+    for(let i=0;i<posAtr.count;i++)
+{
+    let z = posAtr.getZ(i);
+    let vel = velocities[i];
+    const accel = accelerations[i];
+    if(z<-200){
+    
+        z = 200;
+        vel = 0;
+    }
+    vel += accel;
+    velocities[i] = vel;
+    z -= vel;
+    posAtr.setZ(i,z);
+}
+    stars.rotation.z += 0.0002;
+    posAtr.needsUpdate = true;
+}
+
+
+//Add moon
+function addMoon()
+{
+    const moonGeometry = new THREE.SphereGeometry(5, 32, 32); 
+    const moonTexture = new THREE.TextureLoader().load('asset/plutomap2k.jpg'); 
+    const normalTexture = new THREE.TextureLoader().load('asset/plutobump2k.jpg'); 
+    const moonMaterial = new THREE.MeshStandardMaterial({map:moonTexture,normalMap:normalTexture});
+    moon.geometry = moonGeometry;
+    moon.material = moonMaterial;
+    moon.position.set(65,0,0)
+    moon.name = "moon"
+    scene.add( moon );
+}
+//Moon orbit
+function createMoonOrbit(){
+    var quaternion = new THREE.Quaternion();
+    var object = scene.getObjectByName('moon');
+    quaternion.setFromAxisAngle(new THREE.Vector3(0, 1, 0).normalize(), 0.005);
+    object.position.applyQuaternion(quaternion);
+}   
+
+//Add Jupiter
+function addJupiter(){
+    const jupiterGeometry = new THREE.SphereGeometry(25, 320, 320); 
+    const jupiterTexture = new THREE.TextureLoader().load('asset/jupitermap.jpg'); 
+    const jupiterMaterial = new THREE.MeshStandardMaterial({map:jupiterTexture});
+    jupiter.geometry = jupiterGeometry;
+    jupiter.material = jupiterMaterial;
+    jupiter.rotation.set( 0, 0,-Math.PI * 35 / 180 ); 
+    jupiter.name = "jupiter"
+    scene.add( jupiter );
+    jupiter.position.set(0,250,-750);
+}
+function createJupiterOrbit(){
+    var quaternion = new THREE.Quaternion();
+    var object = scene.getObjectByName('jupiter');
+    quaternion.setFromAxisAngle(new THREE.Vector3(0, 1, 0).normalize(), -0.003);
+    object.position.applyQuaternion(quaternion);
+}   
+//Camera movement with scrolling
+function moveCamera() {
+    const t = document.body.getBoundingClientRect().top;
+    camera.position.z = t * -0.015;
+  }
+document.body.onscroll = moveCamera;
+
+function animate()
+{
+    var z = camera.position.z - cube.position.z;
+    if(z>5){
+        cube.visible = false;
+    }
+    else{
+        cube.visible = true;
+    }
+    cube.rotation.y +=0.0008;
+    createMoonOrbit();
+    createJupiterOrbit();
+    starAnimations();
+    moon.rotation.y += 0.005;
+    jupiter.rotation.y -= 0.005;
+    renderer.render(scene,camera);
+    renderer.setClearColor( 0x000000, 0 );
+    requestAnimationFrame(animate);
+}
+addCube();
+moveCamera();
+addJupiter();
+addMoon();
+addStar();
+animate();
